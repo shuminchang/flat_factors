ccb6d5add8883b6773a8749884b0a1012028e652
shuminchang <magic2313@gmail.com>
2024-10-30 17:50:04 +0800

    add floor score, add plotly and refactoring

--- app.py
+++ app.py
@@ -1,68 +1,80 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-# Set page configuration
-st.set_page_config(layout="wide")
 import utils
 
+# Set page configuration
+st.set_page_config(layout="wide")
 
-# Load the apartment data
-apartments_data = pd.read_csv('data/apartment_choice.csv')
+def get_feature_engineered_data():
+    # Load the apartment data
+    apartments_data = pd.read_csv('data/apartment_choice.csv')
+    # Feature Engineering
+    apartments_data['price_per_area'] = apartments_data['rent'] / apartments_data['area']
+    apartments_data['floor_score'] = apartments_data.apply(lambda row: utils.get_floor_score(row['floor'], row['max_floor']), axis=1)
+    return apartments_data
 
-original_apartments_data = apartments_data.copy()
-
-# Feature Engineering
-apartments_data['price_per_square_meter'] = apartments_data['rent'] / apartments_data['area']
-
-# Streamlit App
-st.title("Apartment Scoring Web App")
-
-# Sidebar with weight adjustment sliders
-st.sidebar.header("Adjust Weights")
-with st.sidebar:
+# Update weights based on sidebar sliders
+def adjust_weights():
+    """Create sliders for weight adjustment in the sidebar."""
+    st.sidebar.header("Adjust Weights")
     for feature in utils.INITIALIZE_WEIGHTS.keys():
         initial_value = utils.INITIALIZE_WEIGHTS[feature]
-        utils.INITIALIZE_WEIGHTS[feature] = st.slider(
+        utils.INITIALIZE_WEIGHTS[feature] = st.sidebar.slider(
             f"Weight for {feature}", 
-            0.0, 
             1.0, 
+            10.0, 
             initial_value, 
-            0.01
+            1.0
         )
 
-# Calculate scores with adjusted weights
-scored_apartments = utils.calculate_scores(apartments_data.copy(), utils.INITIALIZE_WEIGHTS)
+def get_prepared_data(data):
+    original_apartments_data = data.copy()
 
-# Sort apartments by score in descending order
-ranked_apartments = scored_apartments.sort_values(by='score', ascending=False)
+    # Calculate scores with adjusted weights
+    scored_apartments = utils.calculate_scores(data.copy(), utils.INITIALIZE_WEIGHTS)
 
-ranked_apartments['item'] = original_apartments_data['item']
-ranked_apartments['rent'] = original_apartments_data['rent']
-ranked_apartments['area'] = original_apartments_data['area']
-ranked_apartments['distance'] = original_apartments_data['distance']
-ranked_apartments['electric_bill'] = original_apartments_data['electric_bill']
-ranked_apartments['water_bill'] = original_apartments_data['water_bill']
-ranked_apartments['other_bill'] = original_apartments_data['other_bill']
-ranked_apartments['garbage_bill'] = original_apartments_data['garbage_bill']
+    # Sort apartments by score in descending order
+    ranked_apartments = scored_apartments.sort_values(by='score', ascending=False)
 
-# Create a link column (modify this to fit your actual link structure)
-# ranked_apartments['link'] = ranked_apartments['item'].apply(lambda x: f"[Link](https://your-apartment-website.com/apartment/{x})")
+    ranked_apartments['item'] = original_apartments_data['item']
+    ranked_apartments['rent'] = original_apartments_data['rent']
+    # ranked_apartments['rent_norm'] = scored_apartments['rent']
+    ranked_apartments['area'] = original_apartments_data['area']
+    ranked_apartments['distance'] = original_apartments_data['distance']
+    ranked_apartments['electric_bill'] = original_apartments_data['electric_bill']
+    ranked_apartments['floor'] = original_apartments_data['floor']
+    ranked_apartments['price_per_area'] = original_apartments_data['price_per_area']
+    ranked_apartments['max_floor'] = original_apartments_data['max_floor']
+    ranked_apartments['city'] = original_apartments_data['city']
 
-tab1, tab2, tab3, tab4 = st.tabs(["Ranked Apartments", "Model Insights", "Clusters", "Model Explain"])
+    apartments_for_display = ranked_apartments[
+            [
+                'item', 
+                'rent',
+                # 'rent_norm', 
+                'area', 
+                'city',
+                'distance', 
+                'floor', 
+                'max_floor', 
+                'score', 
+                'link'
+            ]
+        ]
+    return scored_apartments, ranked_apartments, apartments_for_display
 
-with tab1:
+def display_ranked_apartments(data, data_for_display):
     # Display the ranked apartments with the clickable link column
     st.header("Ranked Apartments")
+
     # Convert DataFrame to markdown format for clickable links
     st.markdown(
-        ranked_apartments[
-            [
-                'item', 'rent', 'area', 'distance', 'electric_bill', 'water_bill', 'other_bill', 'garbage_bill', 'city', 'score', 'link'
-            ]
-        ].to_markdown(index=False)
+        data_for_display.to_markdown(index=False)
         )
+
     # Download button
-    csv = utils.convert_df(ranked_apartments)
+    csv = utils.convert_df(data)
     st.download_button(
         label="Download ranked apartments as CSV",
         data=csv,
@@ -70,34 +82,73 @@ with tab1:
         mime='text/csv',
     )
 
-model, cv_results, X_test, y_test, y_pred, features = utils.train_model(scored_apartments)
+def display_unselected_features(data, data_for_display):
+    st.header("Unselected Features")
 
-with tab2:
+    unselected_features = data.columns.difference(data_for_display.columns).tolist()
+
+    for feature in unselected_features:
+        st.write(f"- {feature}")
+
+def display_model_insights(model, cv_results, y_test, y_pred, features):
     st.header("Model Insights")
     # Display cross-validation metrics
     utils.display_cv_metrics(cv_results)
     utils.display_metrics(y_test, y_pred)
     utils.plot_feature_importances(model, features)
 
-# Add clustering option
-n_clusters = st.sidebar.slider("Number of Clusters", 2, 10, 3, step=1)
-ranked_apartments, kmeans = utils.perform_clustering(ranked_apartments, n_clusters)
-
-with tab3:
-    # Display clusters on a scatter plot
-    st.header("Apartment Clusters")
-    utils.plot_clusters(ranked_apartments)
-
-with tab4:
+def display_model_explanation(model, X_test, ranked_data):
     shap_values = utils.get_shap_explainer(model, X_test)
     utils.plot_shap_summary(shap_values, X_test)
 
     # Display explanations for top 3 apartments
     st.subheader("Explanations for Top Apartments")
-    top_apartments = ranked_apartments.head(3).copy()
+    top_apartments = ranked_data.head(3).copy()
     top_apartments = top_apartments.reset_index()
     for index, apartment in top_apartments.iterrows():
         shap_value = shap_values[index]
         features = X_test.iloc[index]
         explanation = utils.generate_explanation(apartment, shap_value, features)
         st.markdown(explanation)
+
+def display_app():
+    apartments_data = get_feature_engineered_data()
+    
+    scored_apartments, ranked_apartments, apartments_for_display = get_prepared_data(apartments_data)
+    
+    model, cv_results, X_test, y_test, y_pred, features = utils.train_model(scored_apartments)
+
+    # Add clustering option
+    n_clusters = st.sidebar.slider("Number of Clusters", 2, 10, 3, step=1)
+    ranked_apartments, kmeans = utils.perform_clustering(ranked_apartments, n_clusters)
+
+
+    # Create a link column (modify this to fit your actual link structure)
+    # ranked_apartments['link'] = ranked_apartments['item'].apply(lambda x: f"[Link](https://your-apartment-website.com/apartment/{x})")
+
+    tab1, tab2, tab3, tab4, tab5 = st.tabs(
+        ["Ranked Apartments", "Unselected Features", "Clusters", "Model Insights", "Model Explain"]
+    )
+
+    with tab1:
+        display_ranked_apartments(ranked_apartments, apartments_for_display)
+
+    with tab2:
+        display_unselected_features(ranked_apartments, apartments_for_display)
+
+    with tab3:
+        # Display clusters on a scatter plot
+        st.header("Apartment Clusters")
+        utils.plot_clusters(ranked_apartments)
+
+    with tab4:
+        display_model_insights(model, cv_results, y_test, y_pred, features)
+
+
+    with tab5:
+        display_model_explanation(model, X_test, ranked_apartments)
+
+# Streamlit App
+st.title("Apartment Scoring Web App")
+adjust_weights()
+display_app()
--- utils.py
+++ utils.py
@@ -1,89 +1,96 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import shap
+import pandas as pd
+import plotly.express as px
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import cross_validate
 from sklearn.cluster import KMeans
-import streamlit as st
-import matplotlib.pyplot as plt
-import seaborn as sns
-import shap
-import pandas as pd
 
 IMAGE_WIDTH = 1000
 # Initialize weights for selected features
 INITIALIZE_WEIGHTS = {
-    'rent': 0.4,
-    'area': 0.3,
-    'floor': 0.05,
-    'max_floor': 0.05,
-    'deposit': 0.05,
-    'distance': 0.2,
-    'fridge': 0.05,
-    'washing_machine': 0.05,
-    'air_conditioner': 0.05,
-    'bed': 0.05,
-    'water_heater': 0.05,
-    'gas': 0.05,
-    'closet': 0.05,
-    'internet': 0.05,
-    'sofa': 0.05,
-    'table': 0.05,
-    'tv': 0.05,
-    'cable': 0.05,
-    'balcony': 0.05,
-    'park': 0.05,
-    'electric_bill': 0.05,  # Negative weight for higher bills
-    'water_bill': 0.05,      # Negative weight for higher bills
-    'other_bill': 0.05,        # Negative weight for higher bills
-    'garbage_bill': 0.05     # Negative weight for higher bills
+    'rent': 5.0,
+    'area': 4.0,
+    'city': 1.0,
+    'distance': 1.0,
+    'floor_score': 1.0
+    # 'max_floor': 0.05,
+    # 'deposit': 0.05,
+    # 'fridge': 0.05,
+    # 'washing_machine': 0.05,
+    # 'air_conditioner': 0.05,
+    # 'bed': 0.05,
+    # 'water_heater': 0.05,
+    # 'gas': 0.05,
+    # 'closet': 0.05,
+    # 'internet': 0.05,
+    # 'sofa': 0.05,
+    # 'table': 0.05,
+    # 'tv': 0.05,
+    # 'cable': 0.05,
+    # 'balcony': 0.05,
+    # 'park': 0.05,
+    # 'electric_bill': 0.05,  # Negative weight for higher bills
+    # 'water_bill': 0.05,      # Negative weight for higher bills
+    # 'other_bill': 0.05,        # Negative weight for higher bills
+    # 'garbage_bill': 0.05,     # Negative weight for higher bills
 }
 
+def get_floor_score(floor, max_floor):
+    """Calculate a score based on the apartment's floor level."""
+    if floor == -1 or floor > max_floor:
+        return -0.1 * INITIALIZE_WEIGHTS['floor_score']
+    else:
+        return (1 - floor) * INITIALIZE_WEIGHTS['floor_score']
+    
 # Function to calculate scores based on weights
 def calculate_scores(data, weights):
+    """Calculate scores for each apartment based on given weights."""
     # Normalize and score features
-    # max_rent = data['rent'].max()
-    # max_area = data['area'].max()
-    # max_distance = data['distance'].max()
-    
-    # rent_score = ((max_rent - data['rent']) / max_rent) * weights['rent']
-    # area_score = (data['area'] / max_area) * weights['area']
-    # distance_score = ((max_distance - data['distance']) / max_distance) * weights['distance']
     numeric_features = ['rent', 'area', 'floor', 'max_floor', 'deposit', 'distance',
-                    'electric_bill', 'water_bill', 'other_bill', 'garbage_bill']
+                        'electric_bill', 'water_bill', 'other_bill', 'garbage_bill', 
+                        'price_per_area', 'floor_score', 'city']
     scaler = MinMaxScaler()
     data[numeric_features] = scaler.fit_transform(data[numeric_features])
     
     # Compute total score
     data['score'] = (
-        (1 - data['rent']) * weights['rent'] +  # Rent is penalized
-        data['area'] * weights['area'] +       # Area is rewarded
+        (1 - data['rent']) * weights['rent'] +
+        data['area'] * weights['area'] +    # Area is rewarded
+        (1 - data['city']) * weights['city'] +
         (1 - data['distance']) * weights['distance'] +  # Distance is penalized
-        data['air_conditioner'] * weights['air_conditioner'] +
-        data['fridge'] * weights['fridge'] +
-        data['washing_machine'] * weights['washing_machine'] +
-        data['bed'] * weights['bed'] +
-        data['water_heater'] * weights['water_heater'] +
-        data['gas'] * weights['gas'] +
-        data['closet'] * weights['closet'] +
-        data['internet'] * weights['internet'] +
-        data['sofa'] * weights['sofa'] +
-        data['table'] * weights['table'] +
-        data['tv'] * weights['tv'] +
-        data['cable'] * weights['cable'] +
-        data['balcony'] * weights['balcony'] +
-        data['park'] * weights['park'] +
-        (1 - data['electric_bill']) * weights['electric_bill'] +  # Bills penalized
-        (1 - data['water_bill']) * weights['water_bill'] +
-        (1 - data['other_bill']) * weights['other_bill'] +
-        (1 - data['garbage_bill']) * weights['garbage_bill']
+        data['floor_score'] * weights['floor_score']
+
+        # data['air_conditioner'] * weights['air_conditioner'] +
+        # data['fridge'] * weights['fridge'] +
+        # data['washing_machine'] * weights['washing_machine'] +
+        # data['bed'] * weights['bed'] +
+        # data['water_heater'] * weights['water_heater'] +
+        # data['gas'] * weights['gas'] +
+        # data['closet'] * weights['closet'] +
+        # data['internet'] * weights['internet'] +
+        # data['sofa'] * weights['sofa'] +
+        # data['table'] * weights['table'] +
+        # data['tv'] * weights['tv'] +
+        # data['cable'] * weights['cable'] +
+        # data['balcony'] * weights['balcony'] +
+        # data['park'] * weights['park'] +
+        # (1 - data['electric_bill']) * weights['electric_bill'] +  # Bills penalized
+        # (1 - data['water_bill']) * weights['water_bill'] +
+        # (1 - data['other_bill']) * weights['other_bill'] +
+        # (1 - data['garbage_bill']) * weights['garbage_bill'] + 
     )
     return data
 
 def train_model(scored_data, cv_folds=5):
     # Prepare data for the model after scores are calculated
-    # features = scored_apartments[['rent', 'area', 'distance', 'electric_bill', 'water_bill', 'other_bill', 'garbage_bill']]
-    features = scored_data.loc[:, ~scored_data.columns.isin(['link', 'score'])]
+    features = scored_data[['rent', 'area', 'city', 'distance', 'floor_score']]
+    # features = scored_data.loc[:, ~scored_data.columns.isin(['item', 'link', 'score'])]
     target = scored_data['score']
 
     X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
@@ -173,37 +180,79 @@ def convert_df(df):
     return df.to_csv(index=False).encode('utf-8')
 
 def perform_clustering(data, n_clusters=3):
-    features = data[['rent', 'area', 'distance']]
+    features = data[
+        [
+            'rent',
+            'area',
+            'floor_score', 
+            'distance',
+            'city'
+            ]
+        ]
     kmeans = KMeans(n_clusters=n_clusters, random_state=42)
     data['cluster'] = kmeans.fit_predict(features)
     return data, kmeans
 
+# def plot_clusters(data):
+#     # Display clusters on a scatter plot
+#     st.subheader("Apartment Clusters")
+#     plt.figure(figsize=(10, 6))
+#     sns.scatterplot(
+#         data=data, 
+#         x='area', 
+#         y='rent', 
+#         hue='city', 
+#         size='score', 
+#         sizes=(20, 200),
+#         style='cluster',
+#         palette='tab10'
+#     )
+#     plt.title("Apartment Clusters")
+#     # Save the figure to a file
+#     plt.savefig("clusters.png", bbox_inches='tight')
+#     plt.close()
+
+#     # Display the saved image in Streamlit with specific width
+#     st.image("clusters.png", width=IMAGE_WIDTH)  # Adjust width as needed
+
 def plot_clusters(data):
-    # Display clusters on a scatter plot
-    st.subheader("Apartment Clusters")
-    plt.figure(figsize=(10, 6))
-    sns.scatterplot(
-        data=data, 
+    # Ensure data contains a 'link' column for displaying URLs on hover
+    if 'link' not in data.columns:
+        st.write("The dataset doesn't contain a 'link' column.")
+        return
+
+    # Add a minimum size threshold to avoid near-zero sizes
+    min_marker_size = 5
+    data['plot_size'] = data['score'].apply(lambda x: max(x, 0.01)) * 100  # Scaling for visibility
+    data['plot_size'] = data['plot_size'].clip(lower=min_marker_size)  # Ensure a minimum size
+
+     # Create a new column with HTML-formatted clickable links
+    data['clickable_link'] = data['link'].apply(lambda x: f'<a href="{x}" target="_blank">{x}</a>')
+
+    # Display clusters on an interactive scatter plot with hover functionality
+    # st.subheader("Apartment Clusters")
+    fig = px.scatter(
+        data, 
         x='area', 
         y='rent', 
-        hue='cluster', 
-        size='score', 
-        sizes=(20, 200),
-        style='city',
-        palette='tab10'
+        color='city', 
+        size='cluster',
+        size_max=10,
+        hover_data={'clickable_link': True, 'score': True, 'area': True, 'rent': True},
+        title="Apartment Clusters",
+        labels={'area': 'Area (sq ft)', 'rent': 'Rent ($)'}
     )
-    plt.title("Apartment Clusters")
-    # Save the figure to a file
-    plt.savefig("clusters.png", bbox_inches='tight')
-    plt.close()
 
-    # Display the saved image in Streamlit with specific width
-    st.image("clusters.png", width=IMAGE_WIDTH)  # Adjust width as needed
+    fig.update_traces(marker=dict(sizemin=4))
+    fig.update_layout(width=IMAGE_WIDTH, height=600)
+    
+    # Display Plotly chart in Streamlit
+    st.plotly_chart(fig)
 
 def get_shap_explainer(model, X):
     """Create a SHAP explainer for the model."""
     explainer = shap.Explainer(model, X)
-    shap_values = explainer(X)
+    shap_values = explainer(X, check_additivity=False)
     return shap_values
 
 def plot_shap_summary(shap_values, features):
